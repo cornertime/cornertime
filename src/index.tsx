a07e72f1894e1460f8390e9a37a56a2066a0b5e8
Santtu Pajukanta <santtu@pajukanta.fi>
2018-01-16 02:06:09 +0200

    the service worker does not play nice, so unregister it

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import './index.css';
+
 
 ReactDOM.render(
     <App />,
     document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+
+// The service worker was a mistake and now it has been registered to some users already.
+unregister();
